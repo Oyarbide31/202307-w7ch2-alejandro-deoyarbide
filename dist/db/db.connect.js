73925545db5561b9eb4af0d10f155815c1cee18f
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-30 00:29:29 +0200

    Remove dist

--- dist/db/db.connect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const dbConnect = () => {
-    const user = process.env.DB_USER;
-    const passwd = process.env.DB_PASSWD;
-    const uri = `mongodb+srv://${user}:${passwd}@cluster0.dwfebgw.mongodb.net/?retryWrites=true&w=majority`;
-    return mongoose.connect(uri);
-};
--- dist/repository/repository.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-export {};
-/*
-Se descrie un conjunto de métodos para interactuar con una fuente de datos.
-Las implementaciones concretasde esta interfaz se usan para interactuar con diferentes
-tipos de fuentes de datos.
-
-
-*/
